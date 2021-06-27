2ec1b59a91586cf712b56eacd65afda703a7c487
Zephyr Shannon <earthlingzephyr@gmail.com>
2021-06-27 15:12:54 -0700

    Modify the select images to work better with my background

---
+++ my-theme.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo ./install.sh --install --background backgrounds/transbian-pride.png --resolution 3840x2160 --font DejaVuSans.ttf --fontcolour '#131313,#dedede' --fontsize 24 --bold
